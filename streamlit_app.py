80e436c02ed54f9a452d58cc3a48874b88dbdb9b
SuperJuanxo <122353954+SuperJuanxo@users.noreply.github.com>
2023-01-10 10:57:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-stremlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Menu')
+stremlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard- Boiled Free-Range Egg')
