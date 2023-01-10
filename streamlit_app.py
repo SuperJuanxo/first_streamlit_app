1065e820252615bc947f022b29870e515a68cd8f
SuperJuanxo <122353954+SuperJuanxo@users.noreply.github.com>
2023-01-10 10:56:19 +0100

    Update streamlit_app.py
    add header + 3 texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+stremlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard- Boiled Free-Range Egg')
