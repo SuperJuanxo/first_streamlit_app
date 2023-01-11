a997fc99c8e5d35cf2a67a6f8b7cfe7d36d6d787
SuperJuanxo <122353954+SuperJuanxo@users.noreply.github.com>
2023-01-11 11:08:53 +0100

    Update streamlit_app.py
    header to display fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,9 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+#New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
 #Import requests 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
