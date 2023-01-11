1413ea822bf2bba84470b46100949cf2baee097b
SuperJuanxo <122353954+SuperJuanxo@users.noreply.github.com>
2023-01-11 13:26:49 +0100

    Update streamlit_app.py
    adding fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,9 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
 #my_data_row = my_cur.fetchone() -- un valor "fetchONE" ---> fetchALL todos
 my_data_rows = my_cur.fetchall()
-streamlit.header("the fruit load contains:")
+streamlit.header("The fruit load contains:")
 streamlit.dataframe(my_data_rows)
+#Adding fruits
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_frui)
+
